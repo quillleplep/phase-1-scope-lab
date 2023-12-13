61d2f6b39c9a7c70693a7117e986f7de07a24767
quill_nichols <leplep00100@gmail.com>
2023-12-13 12:15:17 -0500

    neat

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else';
+}
+
+
 // Write your solution in this file!
